484759d0a9385a3eb9c879242fe9ec7f25b73a84
Mihai Gătejescu <mgatejescu@pm.me>
2025-08-15 14:37:28 +0300

    Render return the cursor line

--- lua/vim-be-good/games/surround.lua
+++ lua/vim-be-good/games/surround.lua
@@ -93,11 +93,11 @@ end
 
 function SurroundRound:render()
     local lines = GameUtils.createEmpty(gameLineCount)
-    local cursorIdx = 5
+    local cursorLine = 5
 
     lines[5] = table.concat(self.config.words, " ")
 
-    return lines, cursorIdx
+    return lines, cursorLine
 end
 
 
